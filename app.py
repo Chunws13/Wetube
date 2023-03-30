a0045b1c9c01cb78c9ddc8de41d6c587d3da8c32
Chunws13 <dntkd51@naver.com>
2023-03-31 01:28:21 +0900

    2023-03-31 almost complete

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ def home():
             
             # 유저 정보 중 nick_name 반환
             nick_name = user_info['nick_name']
-            return render_template('index.html', status = True, nick_name = nick_name)
+            user_id = user_info['user_id']
+            return render_template('index.html', status = True, nick_name = nick_name, user_id = user_id)
         
         except jwt.ExpiredSignatureError:
             # 토큰 시간이 만료된 경우
@@ -37,6 +38,7 @@ def home():
     else:
         return render_template('index.html')
 
+# 로그인
 @app.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "GET":
@@ -69,11 +71,13 @@ def login():
         # 등록된 유저가 아닌 경우
         else:
             return jsonify({'status' : False, 'message' : "아이디 또는 비밀번호가 잘못되었습니다."})
-    
+
+
+# 회원가입    
 @app.route('/register', methods=["POST", "GET"])
 def register():
     if request.method == "POST":
-        # register.html 파일에서 보내온 회원가입 정보 수신)
+        # register.html 파일에서 보내온 회원가입 정보 수신
         userid_receive = request.form['user_id']
         nickname_receive = request.form['nick_name']
         password1_receive = request.form['password1']
@@ -111,16 +115,45 @@ def register():
     # GET 요청시 회원가입 페이지로 유도
     else:
         return render_template('register.html')
+
+
+# 마이 페이지 접근 로직
+@app.route("/mypage", methods=["GET"])
+def mypage():
+    token_receive = request.cookies.get('token')
+    # 로그인한 사용자만 마이 페이지 접근 버튼이 보이므로, 토큰 값이 없는 경우가 없음, <- home 로직과 다른 부분
+    try:
+        # 토큰 받아와서 
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        
+        # payload 값 중 id로 유저 정보 찾기
+        user_info = db.wetube_user.find_one({'user_id':payload['id']}, {'_id': False})
+        
+        # 유저 정보 중 nick_name 반환, 유저 id 기준 등록 영상 조회를 위해 id 전달
+        nick_name = user_info['nick_name']
+        user_id = user_info['user_id']
+        
+        # 유저가 등록한 글 찾아내기
+        user_content = db.movies.find({"writer" : user_id}, {"_id" : False, "writer": False})
+        return render_template('mypage.html', status = True, nick_name = nick_name, user_id = user_id)
     
-@app.route("/movie", methods=["POST"])
+    except jwt.ExpiredSignatureError:
+        # 토큰 시간이 만료된 경우
+        msg = "로그인 시간이 만료되었습니다."
+        return render_template('index.html', statud = False, message = msg)
+
+
+# 영상 평가 등록하는 기능
+@app.route("/movie/post", methods=["POST"])
 def movie_post():
-    # sample_receive = request.form['sample_give']
+    
     url_receive = request.form['url_give']
+    writer_receive = request.form['writer_give'] # 마이페이지 구현을 위해 작성자 신규 등록
     comment_receive = request.form['comment_give']
     star_receive = request.form['star_give']
+    print(url_receive, writer_receive)
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     data = requests.get(url_receive, headers=headers)
-
     soup = BeautifulSoup(data.text, 'html.parser')
 
     # 여기에 코딩을 해서 meta tag를 먼저 가져와보겠습니다.
@@ -133,32 +166,39 @@ def movie_post():
         'title' : ogtitle,
         'desc' : ogdesc,
         'image' : ogimage,
-        # 'url' : url_receive, # db에 필요한 정보만 빼오는거라 db에 저장은 필요 없다.
+        'writer' : writer_receive,
         'comment' : comment_receive,
-        'star' : star_receive
+        'star' : star_receive,
+        'url' : url_receive 
     }
+    print(doc)
     db.movies.insert_one(doc)
 
     return jsonify({'msg':'저장 완료!'})
 
-@app.route("/movie", methods=["GET"])
+
+# 첫 페이지 & 마이페이지 영상 정보 가져오기
+@app.route("/movie", methods=["GET", "POST"])
 def movie_get():
-
+    
+    # 마이페이지 - 특정 유저의 게시글을 볼 때
+    if request.method == 'POST':
+        writer_receive = request.form['writer_give']
+        all_movies = list(db.movies.find({'writer': writer_receive}))
+        
     # 여러개 찾기 - 예시 ( _id 값은 제외하고 출력)
-    all_movies = list(db.movies.find({},{'_id':False}))
+    else:
+        all_movies = list(db.movies.find({}))
 
-    return jsonify({'result' : all_movies})
+    return jsonify({'result' : dumps(all_movies)})
 
 # 수정하기
 @app.route("/movie/update", methods=["PUT"])
 def movie_put():
+    content_id = request.form['id_give']
     comment_new = request.form['comment_give']
-    star_new = request.form['star_give']
-    
-    print(comment_new)
-    print(star_new)
 
-    db.movies.update_one({}, {'$set': {'comment': comment_new, 'star': star_new}})
+    db.movies.update_one({"_id" : ObjectId(content_id)}, {'$set': {'comment': comment_new}})
 
     return jsonify({'msg':'수정 완료!'})
 
@@ -166,12 +206,20 @@ def movie_put():
 @app.route("/movie/delete", methods=["DELETE"])
 def movie_delete():
         
-        # 삭제할 영화의 title을 가져온다.
-        title_receive = request.form['title_give']
-        # 삭제할 영화의 title을 가진 영화를 찾는다.
-        db.movies.delete_one({'title': title_receive})
+    # 삭제할 영화의 id를 가져온다.
+    id = request.form['id_give']
     
-        return jsonify({'msg':'삭제 완료!'})
+    # 삭제할 영화의 id를 가진 영화를 찾는다.
+    db.movies.delete_one({'_id': ObjectId(id)})
+
+    return jsonify({'msg':'삭제 완료!'})
+
+# 영상 세부 정보 보기 - 추가
+@app.route("/movie/detail/<string:id>")
+def movie_detail(id):
+    movie_detail = list(db.movies.find({'_id' : ObjectId(id)}))
+    return jsonify({"result" : dumps(movie_detail)})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5002, debug=True)
